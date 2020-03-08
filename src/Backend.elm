3b3c4d44d028ddc936c1476b67a8af1712a8c342
Mario Rogic <hello@mario.net.au>
2020-03-08 10:43:57 +0000

    Adjust to keep all attempts, but display only the top for each name
    Lets us keep overall game stats for more fun in future :)

--- src/Backend.elm
+++ src/Backend.elm
@@ -2,8 +2,8 @@ module Backend exposing (..)
 
 import Html
 import Lamdera exposing (ClientId, SessionId)
-import Types exposing (..)
 import List.Extra as List
+import Types exposing (..)
 
 
 type alias Model =
@@ -50,10 +50,9 @@ updateFromFrontend sessionId clientId msg model =
                         |> List.append [ { score | name = String.left 5 score.name } ]
                         |> List.sortBy .score
                         |> List.reverse
-                        |> List.uniqueBy .name
             in
             ( { model | highScores = newScores }
-            , Lamdera.sendToFrontend clientId (TopScores (newScores |> List.take 6))
+            , Lamdera.sendToFrontend clientId (TopScores (newScores |> List.uniqueBy .name |> List.take 6))
             )
 
         NoOpToBackend ->
